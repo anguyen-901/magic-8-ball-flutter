5a5a81849718a3662e8e7c8f50eb7c3ef4673e1d
Albert Nguyen <albertnguyen34@gmail.com>
2023-03-18 16:38:43 -0500

    magic 8 ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,46 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text("Ask Me Anything"),
+            backgroundColor: Colors.blue[900],
+          ),
+          body: Magic8Ball(),
+        ),
       ),
     );
+
+class Magic8Ball extends StatefulWidget {
+  const Magic8Ball({Key key}) : super(key: key);
+
+  @override
+  State<Magic8Ball> createState() => _Magic8BallState();
+}
+
+class _Magic8BallState extends State<Magic8Ball> {
+  int ballNum = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  ballNum = Random().nextInt(5) + 1;
+                });
+              },
+              child: Image.asset("images/ball$ballNum.png"),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
